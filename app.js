d6865ddd4e365a53062362642048077e8d1761eb
ANDRES ORTEGA PABLOS <andres.pablos@ibm.com>
2020-06-29 13:51:09 -0500

    Add hello Mars code

--- app.js
+++ app.js
@@ -22,6 +22,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.use('/mars', function(req, res, next) {
+  res.send('Hello Mars!');
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
